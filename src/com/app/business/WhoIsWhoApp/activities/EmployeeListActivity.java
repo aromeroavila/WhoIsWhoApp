b6f34cf09625e2227fce79509728c32a124b7d64
aromeroavila <aromeroavila@gmail.com>
2014-05-26 12:49:57 +0100

    Added basic UI structure.

--- src/com/app/business/WhoIsWhoApp/activities/EmployeeListActivity.java
+++ src/com/app/business/WhoIsWhoApp/activities/EmployeeListActivity.java
@@ -1,15 +1,16 @@
-package com.app.business.WhoIsWhoApp;
+package com.app.business.WhoIsWhoApp.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.app.business.WhoIsWhoApp.R;
 
-public class MyActivity extends Activity {
+public class EmployeeListActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.employee_list_activity);
     }
 }
